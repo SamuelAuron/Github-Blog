1fe1dfd6a39668fc9c83ded47fbeda5fbc66c55b
Samuel <samueld.r.abreu@gmail.com>
2024-02-24 15:11:19 -0300

    refactor: Update Header component

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,15 +2,12 @@ import styled from 'styled-components';
 
 export const HeaderContainer = styled.header`
   background: ${(props) => props.theme['blue-800']};
-  padding: 1.875rem 0 1.875rem;
+  padding: 1.875rem 0;
 `
 
 export const HeaderContent = styled.div`
   width: 100%;
-  margin: 0;
-  padding: 0;
-
   display: flex;
   justify-content: space-between;
-  align-items: space-around;
+  align-items: center;
 `
