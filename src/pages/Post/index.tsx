39e10b849ce1aa6e421c5f3eaa2db9afcb428930
Samuel <samueld.r.abreu@gmail.com>
2024-02-27 06:25:29 -0300

    feat: Create Post page

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,8 +1,10 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "../../styles/global";
 import { defaultTheme } from "../../styles/themes/default";
+import { Header } from "../../components/Header";
 import { useEffect, useState } from "react";
-import { Container, IssueInfo, LinkContainer, PostContainer, PostContent, PostText } from "./styles";
+import axios from "axios";
+import { IssueInfo, LinkContainer, PostContainer, PostContent, PostText } from "./styles";
 import { formatDistanceToNow } from "date-fns";
 import { ptBR } from "date-fns/locale";
 
@@ -11,41 +13,34 @@ import linkIcon from "../../assets/linkIcon.svg"
 import calender from "../../assets/calender.svg"
 import comments from "../../assets/comments.svg"
 import Markdown from "react-markdown";
-import { getIssueData, getUserData } from "../../services/api";
-import { useParams } from "react-router-dom";
+
 
 interface Issue {
   title: string
   comments: number
   body: string
-  created_at: string
+  created_at: Date
   html_url: string
 }
 
-interface RouteParams {
-  [key: string]: string | undefined
-  issueNum?: string
-}
-
 export function Post() {
-  const { issueNum } = useParams<RouteParams>()
 
-  const [issue, setIssue] = useState<Issue>({
-    title: '',
-    comments: 0,
-    body: '',
-    created_at: '2024-02-19T01:21:27Z',
-    html_url: ''
-  })
+  const [issue, setIssue] = useState<Issue>()
   const [user, setUser] = useState('')
   
+
   useEffect(() => {
     const fetchIssues = async () => {
       try {
-        const responseIssue = await getIssueData(issueNum)
-        const responseUser = await getUserData()
-        setIssue(responseIssue);
-        setUser(responseUser.login)
+        const response = await axios.get('https://api.github.com/repos/SamuelAuron/Github-Blog/issues/1', {
+          //params: {
+          //  q: '',
+          //}
+        });
+        setIssue(response.data);
+        setUser(response.data.user.login)
+        console.log(response.data)
+        console.log(issue?.created_at)
       } catch (error) {
         console.error('Erro ao buscar as issues:', error);
       }
@@ -54,55 +49,47 @@ export function Post() {
     fetchIssues();
   }, []);
 
-  const formattedDate = formatDistanceToNow(new Date(issue.created_at), {
-    locale: ptBR,
-    addSuffix: true,
-  })
-
   return(
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Container>
-      <PostContainer>
-        <LinkContainer>
-          <a href="/">
-            VOLTAR
-          </a>
-          <a href={issue?.html_url}>
-            VER NO GITHUB
-            <img src={linkIcon} alt="" />  
-          </a>
-        </LinkContainer>
+        <Header />
+        <PostContainer>
+          <LinkContainer>
+            <a href="http://localhost:5173/">
+              VOLTAR
+            </a>
+            <a href={issue?.html_url}>
+              VER NO GITHUB
+              <img src={linkIcon} alt="" />  
+            </a>
+          </LinkContainer>
           
-        <PostContent>
-          <h1>{issue?.title}</h1>
-          <div>
-            <IssueInfo>
-              <img src={githubIcon} alt="" />
-              <span>{user}</span>
-            </IssueInfo>
-              
-            <IssueInfo>
-              <img src={calender} alt="" />
-              <span>{formattedDate}</span>
-            </IssueInfo>
-
-            <IssueInfo>
-              <img src={comments} alt="" />
-              <span>
+          <PostContent>
+            <h1>{issue?.title}</h1>
+            <div>
+              <IssueInfo>
+                <img src={githubIcon} alt="" />
+                {user}
+              </IssueInfo>
+              <IssueInfo>
+                <img src={calender} alt="" />
+                {formatDistanceToNow(issue?.created_at, {
+                  locale: ptBR,
+                  addSuffix: true,
+                  })}
+              </IssueInfo>
+              <IssueInfo>
+                <img src={comments} alt="" />
                 {issue?.comments}
                 {' '}
                 comentarios
-              </span>
-            </IssueInfo>
-          </div>
-        </PostContent>
+              </IssueInfo>
+            </div>
+          </PostContent>
         </PostContainer>
         <PostText>
-          <Markdown >{issue?.body}</Markdown>
+        <Markdown>{issue?.body}</Markdown>
         </PostText>
-      </Container>
-      
     </ThemeProvider>   
   )
 }
--- src/pages/Post/styles.ts
+++ src/pages/Post/styles.ts
@@ -1,37 +1,34 @@
 import styled from "styled-components";
 
-export const Container =styled.div`
-  max-width: 70rem;
-  margin: 0 auto;
-`
-
 export const PostContainer = styled.div`
-  width: 100%;
+  max-width: 1120px;
   box-shadow: 0px 2px 28px black;
   padding: 2.5rem 2rem;
+  margin: 0 auto;
   margin-top: -5rem;
+  
   
   background:  ${(props) => props.theme['blue-800']};
   border-radius: 10px;
 
   h1{
     color: ${(props) => props.theme['blue-100']};
-    margin: 1.5rem 0;
   }
 
   div{
     display: flex;
   }
 
+
   a{
     display: flex;
     align-items: center;
     height: min-content;
     text-decoration: none;
-    color: ${(props) => props.theme.blue}; 
+    color: ${(props) => props.theme.blue};    
 
     img{
-      margin-left: 0.5rem;
+      margin-left: 8px;
     }
   }
 
@@ -46,6 +43,7 @@ export const PostContainer = styled.div`
 
 export const LinkContainer = styled.div`
   display: flex;
+  margin: 0;
   justify-content: space-between;
 `
 
@@ -67,6 +65,6 @@ export const IssueInfo = styled.span`
 `
 
 export const PostText = styled.div`
-  max-width: 63.75rem;
-  margin: 2.5rem auto
+  max-width: 1120px;
+  margin: 40px auto
 `
